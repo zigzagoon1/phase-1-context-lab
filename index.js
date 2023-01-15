ed45b86575b2e3596ce789dceedb456c33ea9f8d
KellyMark <kmark602@gmail.com>
2023-01-14 22:35:30 -0500

    commit tree 302d6bc9c65db79496a5bc5d66bb77a5f7784d2f

--- index.js
+++ index.js
@@ -17,7 +17,92 @@ const allWagesFor = function () {
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
+    
     return payable
 }
 
+
+const testEmployee = {
+    firstName: "Kelly",
+    familyName: "Mark",
+    title: "Game Developer",
+    payPerHour: 20,
+    timeInEvents: [],
+    timeOutEvents: []
+}
+
+const createEmployeeRecord = function([firstNameValue, familyNameValue, titleValue, ratePerHour]) {
+    const employeeObj = {
+        firstName: firstNameValue,
+        familyName: familyNameValue,
+        title: titleValue,
+        payPerHour: ratePerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeObj;
+}
+//createEmployeeRecord(["Kelly", "Mark", "Game Developer", 20]);
+
+function createEmployeeRecords(nestedEmployeesArray = []) {
+    let employeeRecords = [];
+    for (const el of nestedEmployeesArray) {
+        const obj = createEmployeeRecord(el);
+        employeeRecords.push(obj);
+    }
+    return employeeRecords;
+}
+
+//createEmployeeRecords([testEmployee, testEmployee])
+
+const createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this;
+}
+//createTimeInEvent("2023-01-14 0900");
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })
+    return this;
+}
+//createTimeOutEvent("2023-01-14 1700")
+
+const hoursWorkedOnDate = function(dateStamp) {
+    const timeIn = this.timeInEvents.find(x => x.date === dateStamp).hour;
+    const timeOut = this.timeOutEvents.find(x => x.date === dateStamp).hour;
+    const hoursWorked = timeOut - timeIn;
+    return hoursWorked / 100;
+}
+//hoursWorkedOnDate("2023-01-14");
+
+const wagesEarnedOnDate = function(dateStamp) {
+    const hours = hoursWorkedOnDate.call(this, dateStamp);
+    return hours * this.payPerHour;
+}
+
+const findEmployeeByFirstName = function(employeeArr, firstName) {
+    for (const employee of employeeArr) {
+        if (employee.firstName === firstName) {
+            return employee;
+        }
+    }
+    return undefined;
+}
+
+function calculatePayroll(employeeRecordsArr) {
+    let totalPay = 0;
+    employeeRecordsArr.reduce((accumulator, currentObj) => {
+        totalPay += allWagesFor.call(currentObj);
+    }, totalPay)
+    return totalPay;  
+}
